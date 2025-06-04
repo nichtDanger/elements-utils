0c740483d527bf8db65294f0b43a79216c2c6ceb
Eposs10 <lunalaluna592@gmail.com>
2025-06-04 13:18:11 +0200

    Init

---
+++ src/client/java/dev/eposs/elementsutils/ElementsUtilsClient.java
@@ -0,0 +1,10 @@
+package dev.eposs.elementsutils;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElementsUtilsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
