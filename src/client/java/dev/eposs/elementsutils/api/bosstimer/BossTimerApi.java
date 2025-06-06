2b82d560611ac4fcc7b4a64daea8a8d75a478d57
Eposs10 <lunalaluna592@gmail.com>
2025-06-06 19:20:39 +0200

    Fix time data and update http client

--- src/client/java/dev/eposs/elementsutils/api/bosstimer/BossTimerApi.java
+++ src/client/java/dev/eposs/elementsutils/api/bosstimer/BossTimerApi.java
@@ -2,36 +2,36 @@ package dev.eposs.elementsutils.api.bosstimer;
 
 import com.google.gson.Gson;
 import dev.eposs.elementsutils.ElementsUtils;
-import dev.eposs.elementsutils.api.HttpHelper;
 import dev.eposs.elementsutils.displays.bosstimer.BossTimerData;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.util.EntityUtils;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class BossTimerApi {
-    private static final URI TIMER_URI = URI.create("https://elements-utils.eposs.dev/api/timer");
-    
+    private static final URI TIMER_URI = URI.create("https://elements-utils.eposs.dev/api/bosstimers");
+
     public static @Nullable BossTimerData getBossTimerData() {
-        try (CloseableHttpResponse response = HttpHelper.request(TIMER_URI)) {
-            if (response.getStatusLine().getStatusCode() != 200) {
-                throw new IOException("Status code " + response.getStatusLine().getStatusCode() + " - " + response.getStatusLine().getReasonPhrase());
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(TIMER_URI)
+                .GET()
+                .build();
+
+        try (HttpClient client = HttpClient.newBuilder().build()) {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() != 200) {
+                throw new IOException("Status code " + response.statusCode() + " - " + response.body());
             }
-            
-            HttpEntity entity = response.getEntity();
-            if (entity == null) {
-                throw new IOException("Response body is empty");
-            }
-            
-            String bodyAsString = EntityUtils.toString(response.getEntity());
-            return new Gson().fromJson(bodyAsString, BossTimerData.class);
-            
+
+            return new Gson().fromJson(response.body(), BossTimerData.class);
+
         } catch (Exception e) {
             ElementsUtils.LOGGER.error("Failed to get boss timer data", e);
             return null;
         }
-    } 
+    }
 }
--- src/client/java/dev/eposs/elementsutils/displays/bosstimer/BossTimerData.java
+++ src/client/java/dev/eposs/elementsutils/displays/bosstimer/BossTimerData.java
@@ -3,28 +3,27 @@ package dev.eposs.elementsutils.displays.bosstimer;
 import dev.eposs.elementsutils.api.bosstimer.BossTimerApi;
 
 import java.time.Instant;
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class BossTimerData {
-    private Instant axolotl;
-    private Instant zombie;
-    private Instant spider;
-    private Instant bogged;
-    private Instant piglin;
+    private String axolotl;
+    private String zombie;
+    private String spider;
+    private String bogged;
+    private String piglin;
 
     private static final AtomicReference<BossTimerData> INSTANCE = new AtomicReference<>(new BossTimerData());
-    
+
     private static Instant lastUpdate = Instant.MIN;
 
     public static void updateData() {
         // Only update data every 10 seconds to prevent spamming the API
         if (lastUpdate.isAfter(Instant.now().minusSeconds(10))) return;
-        
-        Thread.ofVirtual().name("ElementsUtils Boss Timer Data Update Thread").start(() -> {
+
+        Thread.ofVirtual().name("Boss Timer Data Update Thread").start(() -> {
             lastUpdate = Instant.now();
-            
+
             BossTimerData data = BossTimerApi.getBossTimerData();
             if (data != null) {
                 INSTANCE.set(data); // atomic write
@@ -37,27 +36,31 @@ public class BossTimerData {
     }
 
     public ZonedDateTime getAxolotl() {
-        if (axolotl == null) return null;
-        return ZonedDateTime.ofInstant(axolotl, ZoneId.systemDefault());
+        if (axolotl == null || axolotl.isEmpty()) return null;
+        return checkIfValid(ZonedDateTime.parse(axolotl));
     }
-    
+
     public ZonedDateTime getZombie() {
-        if (zombie == null) return null;
-        return ZonedDateTime.ofInstant(zombie, ZoneId.systemDefault());
+        if (zombie == null || zombie.isEmpty()) return null;
+        return checkIfValid(ZonedDateTime.parse(zombie));
     }
-    
+
     public ZonedDateTime getSpider() {
-        if (spider == null) return null;
-        return ZonedDateTime.ofInstant(spider, ZoneId.systemDefault());
+        if (spider == null || spider.isEmpty()) return null;
+        return checkIfValid(ZonedDateTime.parse(spider));
     }
-    
+
     public ZonedDateTime getBogged() {
-        if (bogged == null) return null;
-        return ZonedDateTime.ofInstant(bogged, ZoneId.systemDefault());
+        if (bogged == null || bogged.isEmpty()) return null;
+        return checkIfValid(ZonedDateTime.parse(bogged));
     }
-    
+
     public ZonedDateTime getPiglin() {
-        if (piglin == null) return null;
-        return ZonedDateTime.ofInstant(piglin, ZoneId.systemDefault());
+        if (piglin == null || piglin.isEmpty()) return null;
+        return checkIfValid(ZonedDateTime.parse(piglin));
+    }
+
+    private ZonedDateTime checkIfValid(ZonedDateTime time) {
+        return time.isBefore(ZonedDateTime.ofInstant(Instant.EPOCH, time.getZone())) ? null : time;
     }
 }
