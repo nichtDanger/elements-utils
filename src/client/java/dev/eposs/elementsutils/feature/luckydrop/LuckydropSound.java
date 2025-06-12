7adadcc66679fa9f9e3f6b3103381efd82309ef1
Eposs10 <lunalaluna592@gmail.com>
2025-06-12 18:49:39 +0100

    Adjust sound values

--- src/client/java/dev/eposs/elementsutils/feature/luckydrop/LuckydropSound.java
+++ src/client/java/dev/eposs/elementsutils/feature/luckydrop/LuckydropSound.java
@@ -5,20 +5,24 @@ import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.sound.SoundManager;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
-import org.jetbrains.annotations.NotNull;
 
 public class LuckydropSound {
     public static boolean onGameMessage(Text text, boolean b) {
         if (text.contains(Text.literal("Loot:"))) playsound();
         // TODO: Add all other loot text options
-        
+
         return true;
     }
 
     private static void playsound() {
         SoundManager soundManager = MinecraftClient.getInstance().getSoundManager();
-        soundManager.play(PositionedSoundInstance.master(SoundEvents.BLOCK_NOTE_BLOCK_CHIME, 1.0f));
-        soundManager.play(PositionedSoundInstance.master(SoundEvents.BLOCK_NOTE_BLOCK_CHIME, 1.5f), 2);
-        soundManager.play(PositionedSoundInstance.master(SoundEvents.BLOCK_NOTE_BLOCK_CHIME, 2.0f), 4);
+        float pitch = 1.0F;
+        float pitchIncrease = 0.5F;
+        float volume = 1.0F;
+        int delay = 2;
+
+        soundManager.play(PositionedSoundInstance.master(SoundEvents.BLOCK_NOTE_BLOCK_CHIME.value(), pitch, volume));
+        soundManager.play(PositionedSoundInstance.master(SoundEvents.BLOCK_NOTE_BLOCK_CHIME.value(), pitch + pitchIncrease, volume), delay);
+        soundManager.play(PositionedSoundInstance.master(SoundEvents.BLOCK_NOTE_BLOCK_CHIME.value(), pitch + pitchIncrease * 2, volume), delay * 2);
     }
 }
