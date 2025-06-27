9ff7712d4c8033d8983e519656f4f605c6edca68
Eposs10 <lunalaluna592@gmail.com>
2025-06-27 10:14:15 +0200

    Version 1.4.0

--- src/client/java/dev/eposs/elementsutils/util/DevUtil.java
+++ src/client/java/dev/eposs/elementsutils/util/DevUtil.java
@@ -3,40 +3,51 @@ package dev.eposs.elementsutils.util;
 import dev.eposs.elementsutils.ElementsUtils;
 import dev.eposs.elementsutils.config.ModConfig;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.text.Text;
-import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.item.Items;
 
 public class DevUtil {
     public static void doSomething(MinecraftClient client) {
         if (client.player == null || client.world == null) return;
         if (!ModConfig.getConfig().devUtils.enable) return;
 
-        if (client.crosshairTarget instanceof EntityHitResult entityHitResult) {
-            client.player.sendMessage(Text.literal(entityHitResult.getEntity().getType().toString()), false);
+        ElementsUtils.LOGGER.info("dev");
 
-            if (entityHitResult.getEntity() instanceof VillagerEntity villager) {
-                String text = "...";
-                // text = villager.getCustomName().getString();
+        // PlayerInventory inventory = client.player.getInventory();
+        // ItemStack stack = inventory.getStack(0);
+        // ElementsUtils.LOGGER.info(stack.getItemName().getString());
+        // Item item = stack.getItem();
+        // ElementsUtils.LOGGER.info(item.getTranslationKey());
+        // if (item == Items.POTION) {
+        //     ElementsUtils.LOGGER.info(item.getName().getString());
+        //     ElementsUtils.LOGGER.info(stack.getComponents().toString());
+        // }
 
-                NbtCompound nbt = new NbtCompound();
-                villager.writeNbt(nbt);
-                ElementsUtils.LOGGER.info(nbt.toString());
-
-                ItemStack offHandStack = villager.getOffHandStack();
-                if (offHandStack != null) {
-                    Text customName = offHandStack.getCustomName();
-                    if (customName != null) {
-                        text = customName.getString();
-                    }
-                } else {
-                    text = "none";
-                }
-
-                client.player.sendMessage(Text.literal(text), false);
-            }
-        }
+        // if (client.crosshairTarget instanceof EntityHitResult entityHitResult) {
+        //     client.player.sendMessage(Text.literal(entityHitResult.getEntity().getType().toString()), false);
+        //
+        //     if (entityHitResult.getEntity() instanceof VillagerEntity villager) {
+        //         String text = "...";
+        //         // text = villager.getCustomName().getString();
+        //
+        //         NbtCompound nbt = new NbtCompound();
+        //         villager.writeNbt(nbt);
+        //         ElementsUtils.LOGGER.info(nbt.toString());
+        //
+        //         ItemStack offHandStack = villager.getOffHandStack();
+        //         if (offHandStack != null) {
+        //             Text customName = offHandStack.getCustomName();
+        //             if (customName != null) {
+        //                 text = customName.getString();
+        //             }
+        //         } else {
+        //             text = "none";
+        //         }
+        //
+        //         client.player.sendMessage(Text.literal(text), false);
+        //     }
+        // }
     }
 }
